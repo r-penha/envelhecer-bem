8d1c9658620db559e3acbeeb1d3c0d6b5fbfc105
Rodolfo Penha <rodolfo.penha@gmail.com>
2020-04-07 22:20:47 -0300

    README e snapshots da interface de usuário

--- src/EnvelhecerBem/Startup.cs
+++ src/EnvelhecerBem/Startup.cs
@@ -1,12 +1,7 @@
 using System.ComponentModel;
-using System.Data;
 using System.Globalization;
-using EnvelhecerBem.Core.Data;
-using EnvelhecerBem.Core.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -55,27 +50,6 @@ namespace EnvelhecerBem
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
-        }
-    }
-
-    public static class AppModule
-    {
-        public static IMvcBuilder AddAppModule(this IMvcBuilder builder, IConfiguration configuration)
-        {
-            var connectionString = configuration.GetConnectionString("DefaultConnection");
-            
-            builder.AddApplicationPart(typeof(AppModule).Assembly);
-
-            builder.Services
-                   .AddDbContext<AppDbContext>(o => o.UseSqlServer(connectionString))
-                   .AddScoped<IDbConnection>(c => new SqlConnection(connectionString))
-                   .AddScoped<IUnitOfWork, EfCoreUnitOfWork>()
-                   .AddScoped<IParceiroRepository, ParceiroRepository>()
-                   .AddScoped<IClienteRepository, ClienteRepository>()
-                   .AddScoped<ParceiroApplicationService>()
-                   .AddScoped<ClienteApplicationService>();
-
-            return builder;
         }
     }
 }
